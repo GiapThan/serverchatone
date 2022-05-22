9ddb402a09587e7902c5473508c3d9940b0bb495
Ngân Đặng Thị Kim <giapthan0604@gmail.com>
2022-05-22 15:06:50 +0700

    make it better

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const fs = require('fs')
 const app = express()
 
 const XLSX = require('xlsx')
@@ -8,8 +9,8 @@ const server = require('http').createServer(app)
 const socket = require('./socket').start(server)
 
 const db = require('./config/db')
-const { modelName } = require('./config/models/UserName')
 db.connect()
+const ModelsUserName = require('./config/models/UserName')
 
 app.use(express.urlencoded({
     extended: false
@@ -18,16 +19,13 @@ app.use(express.json())
 
 app.get('/getuser', async (req, res) => {
     var wb = XLSX.utils.book_new()
-    await modelName.find()
-        .then(data => {
-            var temp = JSON.parse(JSON.stringify(data))
-            var ws = XLSX.utils.json_to_sheet(temp)
-            var down = __dirname+'\\download\\demo.xlsx'
-            XLSX.utils.book_append_sheet(wb,ws,'sheet123')
-            XLSX.writeFile(wb,down)
-            res.download(down)
-        })
-        .catch()
+    const data = await ModelsUserName.find()
+    var temp = JSON.parse(JSON.stringify(data))
+    var ws = XLSX.utils.json_to_sheet(temp)
+    var down = __dirname+'\\download\\demo.xlsx'
+    XLSX.utils.book_append_sheet(wb,ws,'sheet123')
+    XLSX.writeFile(wb,down)
+    res.download(down)
 })
 
 //#region 
